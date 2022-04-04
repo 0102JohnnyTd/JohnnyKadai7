2279b8fd2ac1f826aded7b9b170b11cf23f1e99d
0102johnny <rt2824.recruit.1022@gmail.com>
2022-04-05 07:49:38 +0900

    tabBarControllerの実装

---
+++ JohnnyKadai7/AdditionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AdditionViewController.swift
+//  JohnnyKadai7
+//
+//  Created by Johnny Toda on 2022/04/05.
+//
+
+import UIKit
+
+class AdditionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ JohnnyKadai7/SubtractionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SubtractionViewController.swift
+//  JohnnyKadai7
+//
+//  Created by Johnny Toda on 2022/04/05.
+//
+
+import UIKit
+
+class SubtractionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
