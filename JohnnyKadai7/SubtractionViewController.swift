1bc76cade78ff5a2666f823cd75197e9bb3f21db
0102johnny <rt2824.recruit.1022@gmail.com>
2022-04-08 21:45:31 +0900

    引き算に関する処理の追加

--- JohnnyKadai7/SubtractionViewController.swift
+++ JohnnyKadai7/SubtractionViewController.swift
@@ -13,6 +13,8 @@ final class SubtractionViewController: UIViewController {
     @IBOutlet private weak var secondTextField: UITextField!
 
     @IBAction private func executeCalculation(_ sender: Any) {
+        fetchSubtractionResult()
+        view.endEditing(true)
     }
 
     @IBOutlet private weak var resultLabel: UILabel!
@@ -25,7 +27,30 @@ final class SubtractionViewController: UIViewController {
         super.viewDidLoad()
         setUpKeyboard()
     }
+
     private func setUpKeyboard() {
         textfields.forEach{ $0.keyboardType = .numberPad }
+    }
+
+    private func reportingUnderflowWithError(_ result: Int) throws -> Int {
+        guard !(result < Int.min) else {
+            throw UnderflowError()
+        }
+        return result
+    }
+
+    private func fetchSubtractionResult() {
+        do {
+            let values = textfields.map { $0.textToInt }
+            let result = try reportingUnderflowWithError(values[0] - values[1])
+            resultLabel.text = result.description
+        } catch {
+            switch error {
+            case is UnderflowError:
+                resultLabel.text = "値が小さすぎるため扱うことができません"
+            default:
+                resultLabel.text = "不明のエラーです"
+            }
+        }
     }
 }
