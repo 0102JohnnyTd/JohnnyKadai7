4fce7141e36fdf78b877c4799ad48f24eb956bfc
0102johnny <rt2824.recruit.1022@gmail.com>
2022-04-08 21:46:38 +0900

    Errorの構造体を別のファイルに分けた

---
+++ JohnnyKadai7/Error/OverflowError.swift
@@ -0,0 +1,10 @@
+//
+//  OverflowError.swift
+//  JohnnyKadai7
+//
+//  Created by Johnny Toda on 2022/04/08.
+//
+
+import Foundation
+
+struct OverflowError: Error {}
---
+++ JohnnyKadai7/Error/UnderflowError.swift
@@ -0,0 +1,10 @@
+//
+//  UnderflowError.swift
+//  JohnnyKadai7
+//
+//  Created by Johnny Toda on 2022/04/08.
+//
+
+import Foundation
+
+struct UnderflowError: Error {}
