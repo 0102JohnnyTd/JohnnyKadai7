c677dc5e46e68278d4ac2212529c6918613e6981
0102johnny <rt2824.recruit.1022@gmail.com>
2022-04-13 07:50:55 +0900

    UnderflowErrorがうまく機能しなかったので削除

--- JohnnyKadai7/SubtractionViewController.swift
+++ JohnnyKadai7/SubtractionViewController.swift
@@ -7,11 +7,6 @@
 
 import UIKit
 
-private enum UnderflowErrorMessage {
-    static let valueIsTooSmall = "値が大きすぎるため扱うことができません"
-    static let unknownError = "不明のエラーです"
-}
-
 final class SubtractionViewController: UIViewController {
     @IBOutlet private var valueTextFields: [UITextField] = []
 
@@ -31,22 +26,9 @@ final class SubtractionViewController: UIViewController {
         valueTextFields.forEach { $0.keyboardType = .numberPad }
     }
 
-    private func reportingUnderflowWithError(_ result: Int) throws -> Int {
-        guard !(result < Int.min) else {
-            throw UnderflowError()
-        }
-        return result
-    }
-
     private func calculateSubtractionResult() {
-        do {
-            let values = valueTextFields.map { $0.textToInt }
-            let result = try reportingUnderflowWithError(values[0] - values[1])
-            resultLabel.text = result.description
-        } catch is UnderflowError {
-                resultLabel.text = UnderflowErrorMessage.valueIsTooSmall
-        } catch {
-                resultLabel.text = UnderflowErrorMessage.unknownError
-            }
-        }
+        let values = valueTextFields.map { $0.textToInt }
+        let result = values[0] - values[1]
+        resultLabel.text = result.description
+    }
 }
